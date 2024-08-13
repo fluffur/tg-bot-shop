533a763596b80f882a74c363bca88a4747020efe
fluffur <yoworuy@gmail.com>
2024-08-13 17:47:06 +0500

    small code corrections

--- index.js
+++ index.js
@@ -3,22 +3,30 @@ const {Bot} = require("./app/Bot");
 const {Product} = require("./app/Models/Product");
 const {Chat} = require("./app/Models/Chat");
 const {ProductsNotifier} = require("./app/ProductsNotifier");
+
 require('dotenv').config();
+
+
 async function run() {
+
     const db = new Database({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
     });
+
     const chatModel = new Chat(db);
-    await chatModel.createTable();
     const productModel = new Product(db);
+
+    await chatModel.createTable();
     await productModel.createTable();
-    const bot = new Bot(process.env.BOT_TOKEN, );
+
+    const bot = new Bot(process.env.BOT_TOKEN);
     const job = new ProductsNotifier(bot.bot, chatModel, productModel);
+
     job.schedule();
     await bot.start();
 }
 
-(async () => run())()
+(async () => run())();
